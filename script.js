7db2b54667d32b3087cca13194bd92a82f5c320d
fabio-souza <101989744+fabio-leitesouza@users.noreply.github.com>
2023-01-27 19:36:29 +0000

    remodelando

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 /* Definir a largura da barra lateral para px e a margem esquerda do conteúdo da página para px */
 function openNav() {
+  
   document.getElementById("mySidebar").style.width = "250px";
   document.getElementById("main").style.marginLeft = "250px";
 }
 
 /* Definir a largura da barra lateral como 0 e a margem esquerda do conteúdo da página como 0 */
 function closeNav() {
-  document.getElementById("mySidebar").style.width = "0";
-  document.getElementById("main").style.marginLeft = "0";
+  document.getElementById("mySidebar").style.width = "50px";
+  document.getElementById("main").style.marginLeft = "50px";
 }
